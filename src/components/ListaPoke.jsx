c650c87c67c4d939a8431c90319e8178fa3ac7b5
marco.burrometo@gmail.com <marco.burrometo@gmail.com>
2022-03-02 12:37:59 +0100

    02-03

--- src/components/ListaPoke.jsx
+++ src/components/ListaPoke.jsx
@@ -1,35 +1,56 @@
-import { useState } from "react";
-import lista from '../constants/pokemonList'
+import { useEffect, useState } from "react";
 import Input from "./input/input";
 import "./ListaPoke.scss"
 import PokeCard from './PokeCard';
+import { Audio } from 'react-loader-spinner'
 
 function ListaPoke() {
     const [filtro, setFiltro] = useState("");
-    const listaFiltrata = lista.filter(item => {
-        return (
-            item.name.toLowerCase().indexOf(filtro.toLowerCase()) > -1
-        )
-    });
+    const [lista, setLista] = useState([]);
+    const [loading, setloading] = useState(true);
+
+    const fetchPoke = () => {
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=100')
+            .then(response => {
+                return response.json();
+            }).then(data => {
+                // Questi sono i risultati effettivi
+                console.log(data);
+                setLista(data.results);
+                setloading(false);
+            }).catch(err => {
+                console.error('ops', err);
+            });
+    };
+
+    // All'avvio, chiamo API per lista
+    useEffect(() => {
+        fetchPoke();
+    }, []);
 
     return (
         <div className='container-list'>
             {/* <input value={filtro} onChange={function (ev) {
                 setFiltro(ev.target.value.toUppercase());
             }} /> */}
-            <Input value={filtro} onChange={setFiltro} />
 
-            {
-                listaFiltrata.map((item) => (
-                    <PokeCard
-                        key={item.name}
-                        name={item.name}
-                        image={item.image}
-                        types={item.types}
-                    />
-                )
-                )
-            }
+            {loading ? (<Audio color="#00BFFF" height={120} width={120} />) : (
+                <>
+                    <Input value={filtro} onChange={(nuovoValore) => {
+                        setFiltro(nuovoValore);
+                    }} />
+                    {
+                        lista.map((item) => (
+                            <PokeCard
+                                key={item.name}
+                                url={item.url}
+                                nascosto={item.name.toLowerCase().indexOf(filtro.toLowerCase()) === -1}
+                            />
+                        ))
+                    }
+                </>
+            )}
+
         </div>
 
     )
--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,17 +1,65 @@
+import { useEffect, useState } from "react";
 import Badge from "./badge/badge";
+import { Audio } from 'react-loader-spinner'
 import "./PokeCard.scss"
 
+// Se ho il mouse sopra alla PokeCard
+// voglio vedere l'immagine front_shiny
+// altrimenti voglio vedere l'immagine front_default
 
-function PokeCard({ name, image, types }) {
+function PokeCard({ url, nascosto }) {
+    const [data, setData] = useState();
+    const [loading, setloading] = useState(true);
+    const [mouseOver, setMouseOver] = useState(false);
+    const fetchPokeDetails = () => {
+        fetch(url)
+            .then(response => {
+                return response.json();
+            }).then(remoteData => {
+                // Questi sono i risultati effettivi
+                console.log(remoteData);
+                setData(remoteData);
+                setloading(false);
+            }).catch(err => {
+                console.error('ops', err);
+            });
+    };
+    useEffect(() => {
+        fetchPokeDetails();
+    }, [url]);
+
+    if (nascosto) {
+        return null;
+    }
+
+    if (loading) {
+        return (
+            <div className='item-list'>
+                <Audio color="#ff00aa" height={40} width={40} />
+            </div>
+        );
+    }
+
     return (
-        <div className='item-list'>
+        <div
+            className="item-list"
+            onMouseOver={() => {
+                setMouseOver(true);
+            }}
+            onMouseOut={() => {
+                setMouseOver(false);
+            }}
+        >
             <div>
-                <h1 className="title">{name}</h1>
+                <h1 className="title">{data.name} {mouseOver ? '*' : ''}</h1>
             </div>
             <div className='properties'>
-                <img src={image} alt={name}></img>
+                <img
+                    src={mouseOver ? data.sprites.back_default : data.sprites.front_default}
+                    alt={data.name}
+                />
                 <div className="type">
-                    {types.map((item) => {
+                    {data.types.map((item) => {
                         return (
                             <Badge key={item.type.name}>
                                 {item.type.name}
